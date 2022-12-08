a39192b039cbbc093419d5cbde9980899698028a
Andrea-Bighelli <117951296+Andrea-Bighelli@users.noreply.github.com>
2022-12-08 20:37:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main(){
   char b=0;
-  cin>>b;
-  if (convert(b)){
-    cout << b << endl;
-  }else{
-    cout << "errore" << endl;
-  }
+    cin>>b;
+    if (convert(b)){
+      cout << b << endl;
+    }else{
+      cout << "errore" << endl;
+    }
   return 0;
 }
