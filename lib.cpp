ee8e8db3ec0e3796469c825b78e1b46d720a0d49
Andrea-Bighelli <117951296+Andrea-Bighelli@users.noreply.github.com>
2022-12-08 20:33:22 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
-
+#include "lib.h"
+bool convert (char &b){
+  if (b>=65 && b<=90){
+    b +=32;
+    return true;
+  }else if(b>=97 && b<=122){
+    b-=32;
+    return true;
+  }
+  return false;
+}
