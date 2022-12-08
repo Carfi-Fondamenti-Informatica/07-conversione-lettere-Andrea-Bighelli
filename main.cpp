bce0261ed31b68d563aba3d40a201b6605510811
Andrea-Bighelli <117951296+Andrea-Bighelli@users.noreply.github.com>
2022-12-08 20:23:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  char b=0;
+  cin >> b;
+  if (convert (b)){
+    cout << b << endl;
+  }else{
+    cout << "errore" << endl;
+  }
   return 0;
 }
